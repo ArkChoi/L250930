db80c8db43915be87406e675bd2795433cc3cf95
ArkChoi <ark.goya@gmail.com>
2025-09-30 09:40:32 +0900

    N_NPOT_XOR

--- main.cpp
+++ main.cpp
@@ -2,20 +2,38 @@
 
 using namespace std;
 
-int NumberInput();
+int SomePOTNumberChange(int NPOT);
+int Double(int POT);
 
 int main()
 {
 	int GetNumberMax = 0;
 	int NPOTNumber = 0;
+	int POTNumber = 0;
 	int XORRememberNumber = 0;
 
 	cin >> GetNumberMax;
 	for(int i=0;i<GetNumberMax;i++)
 	{
 		cin >> NPOTNumber;
-		XORRememberNumber = XORRememberNumber ^ NPOTNumber;
+		POTNumber = SomePOTNumberChange(NPOTNumber);
+		XORRememberNumber = XORRememberNumber ^ POTNumber;
 	}
 	cout << XORRememberNumber;
 	return 0;
+}
+
+int SomePOTNumberChange(int NPOT) 
+{
+	int POT = 2;
+	while(NPOT > POT)
+	{
+		POT = Double(POT);
+	}
+	return POT;
+}
+
+int Double(int POT)
+{
+	return POT * 2;
 }
